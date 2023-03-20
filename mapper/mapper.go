a75851861012028a34b20aae93b67770cb461d9c
badbubble <liuhanjun369@gmail.com>
2023-03-20 19:40:47 +0800

    define mapper interface

---
+++ mapper/mapper.go
@@ -0,0 +1,8 @@
+package mapper
+
+type Mapper interface {
+	PPUMapRead(addr uint16) (bool, uint16)
+	PPUMapWrite(addr uint16) (bool, uint16)
+	CPUMapRead(addr uint16) (bool, uint16)
+	CPUMapWrite(addr uint16) (bool, uint16)
+}
