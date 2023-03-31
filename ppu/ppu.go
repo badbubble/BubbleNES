695d2e4dbfbd00818b5bcd9b12f5f9e5b02de9de
badbubble <liuhanjun369@gmail.com>
2023-03-31 18:05:13 +0800

    support scroll

--- ppu/ppu.go
+++ ppu/ppu.go
@@ -4,26 +4,24 @@ import (
 	"Nes/cartridge"
 	"Nes/ppu/register"
 	"Nes/ppu/render"
-	"math/rand"
+	"image"
+	"image/color"
 )
 
 const (
 	PaletteTableSize = 32
-	VRamSize         = 2048
 	OMADataSize      = 256
 	FrameWidth       = 256
 	FrameHeight      = 240
+	OneKiB           = 1024
 )
 
 type PPU struct {
-	PatternTable      [2][4096]uint8          //Pattern Tables, Address: 0x0000~0x1FFF
-	NameTable         [2][1024]uint8          // Name Tables, Address: 0x2000~0x3EFF
-	PaletteTable      [PaletteTableSize]uint8 // Palettes, Address: 0x3F00~0x3FFF
-	Frame             [FrameWidth][FrameHeight][3]uint8
-	PatternTableImage [2][128][128][3]uint8
-	NameTableFrame    []uint8
-	PatternTableFrame []uint8
-
+	PatternTable       [2][OneKiB * 4]uint8    //Pattern Tables, Address: 0x0000~0x1FFF
+	NameTable          [2][OneKiB]uint8        // Name Tables, Address: 0x2000~0x3EFF
+	PaletteTable       [PaletteTableSize]uint8 // Palettes, Address: 0x3F00~0x3FFF
+	Frame              *image.RGBA             //[FrameWidth][FrameHeight][3]uint8
+	PatternTableImage  [2]*image.RGBA
 	InternalDataBuffer uint8
 	// Registers
 	Controller register.ControllerRegister // 0x2000
@@ -52,57 +50,148 @@ type PPU struct {
 	BGNextTileAttrib uint8
 	BGNextTileLSB    uint8
 	BGNextTileMSB    uint8
-}
 
-func (p *PPU) SetFrame(x, y int, rgb [3]uint8) {
-	if x >= 0 && x < FrameWidth && y >= 0 && y < FrameHeight {
-		p.Frame[x][y] = rgb
-	}
+	BGShifterPatternLow    uint16
+	BGShifterPatternHigh   uint16
+	BGShifterAttributeLow  uint16
+	BGShifterAttributeHigh uint16
 }
 
 func (p *PPU) Clock() {
-	//if p.Scanline >= -1 && p.Scanline < 240 {
-	//	if p.Scanline == -1 && p.Cycles == 1 {
-	//		p.Status.SetVBlankStatus(false)
-	//	}
-	//	if (p.Cycles >= 2 && p.Cycles <= 258) || (p.Cycles >= 321 && p.Cycles < 338) {
-	//		switch (p.Cycles - 1) % 8 {
-	//		case 0:
-	//			p.BGNextTileId = p.PPURead(0x2000 | (p.VRamAddr.Data & 0x0FFF))
-	//		case 2:
-	//			//p.BGNextTileAttrib = p.PPURead(0x23C0 | (p.VRamAddr.NameTableY << 11))
-	//		case 4:
-	//		case 6:
-	//		case 7:
-	//
-	//		}
-	//	}
-	//	if p.Cycles == 256 {
-	//
-	//	}
-	//}
-	//
-	//if p.Scanline == 241 && p.Cycles == 1 {
-	//	p.Status.SetVBlankStatus(true)
-	//	if p.Controller.GenerateVBlankNMI() {
-	//		p.NMI = true
-	//	}
-	//}
-	if rand.Int()%2 == 0 {
-		p.SetFrame(p.Cycles-1, p.Scanline, render.SystemPalette[0x3F])
-	} else {
-		p.SetFrame(p.Cycles-1, p.Scanline, render.SystemPalette[0x30])
+	if p.Scanline >= -1 && p.Scanline < 240 {
+		if p.Scanline == 0 && p.Cycles == 0 {
+			p.Cycles = 1
+		}
+		if p.Scanline == -1 && p.Cycles == 1 {
+			p.Status.SetVBlankStatus(false)
+		}
+		if (p.Cycles >= 2 && p.Cycles < 258) || (p.Cycles >= 321 && p.Cycles < 338) {
+			p.UpdateShifters()
+			switch (p.Cycles - 1) % 8 {
+
+			case 0:
+				p.LoadBackgroundShifters()
+				// fetch next tile id from nametables
+				// nametable_y(1) + nametable_x(1) + coarse_y(5) + coarse_x(5)
+				p.BGNextTileId = p.PPURead(0x2000 | (p.VRamAddr.Data & 0x0FFF))
+				break
+			case 2:
+				// fetch next tile attribute
+				// 2x32 = 64 attribute
+				// 32 x 32 split into 8 x 8 x ( 4 x 4 ), which means that 4x4 tiles share one attribute byte
+				// 32 x 32 / 4 = 8 x 8
+				// attribute memory starts at 0x23C0
+				// The attribute byte is assembled thus: BR(76) BL(54) TR(32) TL(10)
+				//
+				// +----+----+			    +----+----+
+				// | TL | TR |			    | ID | ID |
+				// +----+----+ where TL =   +----+----+
+				// | BL | BR |			    | ID | ID |
+				// +----+----+			    +----+----+
+				p.BGNextTileAttrib = p.PPURead(0x23C0 | (uint16(p.VRamAddr.GetNameTableY()) << 11) |
+					uint16(p.VRamAddr.GetNameTableX())<<10 |
+					(uint16(p.VRamAddr.GetCoarseY())>>2)<<3 |
+					uint16(p.VRamAddr.GetCoarseY())>>2)
+				if p.VRamAddr.GetCoarseY()&0x02 != 0 {
+					p.BGNextTileAttrib >>= 4
+				}
+				if p.VRamAddr.GetCoarseX()&0x02 != 0 {
+					p.BGNextTileAttrib >>= 2
+				}
+				p.BGNextTileAttrib &= 0x03
+				break
+			case 4:
+				// Fetch the next background tile LSB bit plane from the pattern memory
+				p.BGNextTileLSB = p.PPURead(p.Controller.GetBackgroundPatternAddress() + uint16(p.BGNextTileId)<<4 +
+					uint16(p.VRamAddr.GetFineY()) + 0)
+				break
+			case 6:
+				// Fetch the next background tile MSB bit plane from the pattern memory
+				p.BGNextTileMSB = p.PPURead(p.Controller.GetBackgroundPatternAddress() + uint16(p.BGNextTileId)<<4 +
+					uint16(p.VRamAddr.GetFineY()) + 8)
+				break
+			case 7:
+				p.IncrementScrollX()
+				break
+			}
+		}
+		if p.Cycles == 256 {
+			p.IncrementScrollY()
+		}
+		if p.Cycles == 257 {
+			p.LoadBackgroundShifters()
+			p.TransferAddressX()
+		}
+		if p.Cycles == 338 || p.Cycles == 340 {
+			p.BGNextTileId = p.PPURead(0x2000 | (p.VRamAddr.Data & 0x0FFF))
+		}
+
+		if p.Scanline == -1 && p.Cycles >= 280 && p.Cycles < 305 {
+			p.TransferAddressY()
+		}
 	}
-	p.Cycles += 1
-	if p.Cycles >= 340 {
-		p.Cycles = 0
-		p.Scanline += 1
-		if p.Scanline >= 260 {
-			p.Scanline = 0
-			p.IsFrameComplete = true
+	if p.Scanline == 240 {
+
+	}
+
+	if p.Scanline >= 241 && p.Scanline < 261 {
+		if p.Scanline == 241 && p.Cycles == 1 {
+			p.Status.SetVBlankStatus(true)
+			if p.Controller.GenerateVBlankNMI() {
+				p.NMI = true
+			}
 		}
 	}
 
+	var bgPixel uint8
+	var bgPalette uint8
+
+	if p.Mask.IsShowBG() {
+		bitMux := uint16(0x8000 >> p.FineX)
+		var p0Pixel uint8
+		var p1Pixel uint8
+
+		if p.BGShifterPatternLow&bitMux > 0 {
+			p0Pixel = 1
+		} else {
+			p0Pixel = 0
+		}
+
+		if p.BGShifterPatternHigh&bitMux > 0 {
+			p1Pixel = 1
+		} else {
+			p1Pixel = 0
+		}
+		bgPixel = (p1Pixel << 1) | p0Pixel
+
+		var p0Pal uint8
+		var p1Pal uint8
+
+		if p.BGShifterAttributeLow&bitMux > 0 {
+			p0Pal = 1
+		} else {
+			p0Pal = 0
+		}
+
+		if p.BGShifterAttributeHigh&bitMux > 0 {
+			p1Pal = 1
+		} else {
+			p1Pal = 0
+		}
+		bgPalette = (p1Pal << 1) | p0Pal
+	}
+	p.Frame.Set(p.Cycles-1, p.Scanline, p.GetColorFromPalette(int(bgPalette), bgPixel))
+	//fmt.Printf("Scanline:%d Cycles:%d NTID:%d NTAID:%d\n", p.Scanline, p.Cycles, p.BGNextTileId, p.BGNextTileAttrib)
+	p.Cycles += 1
+
+	if p.Cycles >= 341 {
+		p.Cycles = 0
+		p.Scanline += 1
+		if p.Scanline >= 261 {
+			p.Scanline = -1
+			p.IsFrameComplete = true
+		}
+	}
 }
 
 func (p *PPU) WriteToController(value uint8) {
@@ -110,7 +199,7 @@ func (p *PPU) WriteToController(value uint8) {
 	p.Controller.Update(value)
 	//if !isNMI && p.Controller.GenerateVBlankNMI() && p.Status.IsInVBlank() {
 	//	p.NMI = 1
-	//}
+	//}p
 }
 
 func (p *PPU) WriteToMask(value uint8) {
@@ -202,18 +291,23 @@ func (p *PPU) GetPatternTable(i, palette int) {
 				tileLSB := p.PPURead(uint16(i*0x1000 + nOffset + row + 0x0000))
 				tileMSB := p.PPURead(uint16(i*0x1000 + nOffset + row + 0x0008))
 				for col := 0; col < 8; col++ {
-					pixel := ((tileLSB & 0x01) << 1) | (tileMSB & 0x01)
+					//pixel := ((tileLSB & 0x01) << 1) | (tileMSB & 0x01)
+					pixel := tileMSB&0x01 + tileLSB&0x01
 					tileMSB >>= 1
 					tileLSB >>= 1
-					p.PatternTableImage[i][nTileX*8+(7-col)][nTileY*8+row] = p.GetColorFromPalette(palette, pixel)
+
+					p.PatternTableImage[i].Set(nTileX*8+(7-col), nTileY*8+row, p.GetColorFromPalette(palette, pixel))
+					//fmt.Printf("x:%d y:%d offset:%d msb:%X lsb:%X palette:%d pixel:%d r:%X g:%X b:%X address:%X Resp:%X\n", nTileX*8+(7-col), nTileY*8+row, nOffset, tileMSB, tileLSB, palette, pixel,
+					//	p.GetColorFromPalette(palette, pixel).R, p.GetColorFromPalette(palette, pixel).G, p.GetColorFromPalette(palette, pixel).B, 0x3F00+(uint16(palette)<<2)+uint16(pixel), p.PPURead(0x3F00+(uint16(palette)<<2)+uint16(pixel))&0x3F)
+
 				}
 			}
 		}
 	}
 }
 
-func (p *PPU) GetColorFromPalette(palette int, pixel uint8) [3]uint8 {
-	return render.SystemPalette[p.PPURead(0x3F00+(uint16(palette)<<2)+uint16(pixel))]
+func (p *PPU) GetColorFromPalette(palette int, pixel uint8) color.RGBA {
+	return render.SystemPalette[p.PPURead(0x3F00+(uint16(palette)<<2)+uint16(pixel))&0x3F]
 }
 
 // PPURead PPU read its own address map
@@ -304,7 +398,7 @@ func (p *PPU) PPURead(addr uint16) uint8 {
 		if addr == 0x001C {
 			addr = 0x000C
 		}
-		return p.PaletteTable[addr]
+		return p.PaletteTable[addr] & 0x3F
 	}
 	return 0x0000
 }
@@ -312,6 +406,7 @@ func (p *PPU) PPURead(addr uint16) uint8 {
 // PPUWrite PPU Writes to its own memory map
 func (p *PPU) PPUWrite(addr uint16, data uint8) {
 	// the max ppu memory map address is 0x3FFF
+
 	addr &= 0x3FFF
 	// cartridge map the address
 	if isPPU := p.Cart.PPUWrite(addr, data); isPPU {
@@ -400,8 +495,8 @@ func (p *PPU) CPUWrite(addr uint16, data uint8) {
 	switch addr {
 	case 0x0000: // Control
 		p.WriteToController(data)
-		p.TRamAddr.NameTableX = p.Controller.GetNameTableX()
-		p.TRamAddr.NameTableY = p.Controller.GetNameTableY()
+		p.TRamAddr.SetNameTableX(p.Controller.GetNameTableX())
+		p.TRamAddr.SetNameTableY(p.Controller.GetNameTableY())
 		break
 	case 0x0001: // Mask
 		p.WriteToMask(data)
@@ -414,17 +509,17 @@ func (p *PPU) CPUWrite(addr uint16, data uint8) {
 	case 0x0005: // Scroll
 		if !p.AddressLatch {
 			p.FineX = data & 0x07
-			p.TRamAddr.CoarseX = data >> 3
+			p.TRamAddr.SetCoarseX(data >> 3)
 			p.AddressLatch = true
 		} else {
-			p.TRamAddr.FineY = data & 0x07
-			p.TRamAddr.CoarseY = data >> 3
+			p.TRamAddr.SetFineY(data)
+			p.TRamAddr.SetCoarseY(data >> 3)
 			p.AddressLatch = false
 		}
 		break
 	case 0x0006: // PPU Address
 		if !p.AddressLatch {
-			p.TRamAddr.Data = (p.TRamAddr.Data & 0x00FF) | (uint16(data) << 8)
+			p.TRamAddr.Data = (uint16(data)&0x3F)<<8 | (p.TRamAddr.Data & 0x00FF)
 			p.AddressLatch = true
 		} else {
 			p.TRamAddr.Data = (p.TRamAddr.Data & 0xFF00) | uint16(data)
@@ -432,11 +527,12 @@ func (p *PPU) CPUWrite(addr uint16, data uint8) {
 			p.AddressLatch = false
 		}
 	case 0x0007: // PPU Data
+
 		p.PPUWrite(p.VRamAddr.Data, data)
 		p.VRamAddr.Data += uint16(p.Controller.VRAMAddressIncrement())
 	}
-
 }
+
 func (p *PPU) CPURead(addr uint16) uint8 {
 	var data uint8
 	switch addr {
@@ -447,7 +543,9 @@ func (p *PPU) CPURead(addr uint16) uint8 {
 		p.Status.SetVBlankStatus(false)
 		p.AddressLatch = false
 	case 0x0002: // Status
-		break
+		data = p.Status.Status&0xE0 | p.InternalDataBuffer&0x1F
+		p.Status.SetVBlankStatus(false)
+		p.AddressLatch = false
 	case 0x0003: // OAM Address
 		break
 	case 0x0004: // OAM Data
@@ -479,6 +577,102 @@ func New(cart *cartridge.Cartridge) *PPU {
 			IsHighByte: true,
 		},
 		Cart: cart,
+		PatternTableImage: [2]*image.RGBA{image.NewRGBA(image.Rect(0, 0, 128, 128)),
+			image.NewRGBA(image.Rect(0, 0, 128, 128))},
+		Frame: image.NewRGBA(image.Rect(0, 0, FrameWidth, FrameHeight)),
 	}
 	return p
+}
+
+func (p *PPU) IncrementScrollX() {
+	if p.Mask.IsShowBG() || p.Mask.IsShowSP() {
+		// 32 * 30
+		if p.VRamAddr.GetCoarseX() == 31 {
+			p.VRamAddr.SetCoarseX(0x00)
+			p.VRamAddr.SetNameTableX(^p.VRamAddr.GetNameTableX())
+		} else {
+			p.VRamAddr.SetCoarseX(p.VRamAddr.GetCoarseX() + 1)
+		}
+	}
+}
+
+func (p *PPU) IncrementScrollY() {
+	if p.Mask.IsShowBG() || p.Mask.IsShowSP() {
+		if p.VRamAddr.GetFineY() < 7 {
+			p.VRamAddr.SetFineY(p.VRamAddr.GetFineY() + 1)
+		} else {
+			p.VRamAddr.SetFineY(0x00)
+			if p.VRamAddr.GetCoarseY() == 29 {
+				p.VRamAddr.SetCoarseY(0x00)
+				p.VRamAddr.SetNameTableY(^p.VRamAddr.GetNameTableY())
+			} else if p.VRamAddr.GetCoarseY() == 31 {
+				p.VRamAddr.SetCoarseY(0x00)
+			} else {
+				p.VRamAddr.SetCoarseY(p.VRamAddr.GetCoarseY() + 1)
+			}
+		}
+	}
+}
+
+func (p *PPU) TransferAddressX() {
+	if p.Mask.IsShowBG() || p.Mask.IsShowSP() {
+		p.VRamAddr.SetNameTableX(p.TRamAddr.GetNameTableX())
+		p.VRamAddr.SetCoarseX(p.TRamAddr.GetCoarseX())
+	}
+}
+
+func (p *PPU) TransferAddressY() {
+	if p.Mask.IsShowBG() || p.Mask.IsShowSP() {
+		p.VRamAddr.SetNameTableY(p.TRamAddr.GetNameTableY())
+		p.VRamAddr.SetCoarseY(p.TRamAddr.GetCoarseY())
+		p.VRamAddr.SetFineY(p.TRamAddr.GetFineY())
+
+	}
+}
+
+func (p *PPU) LoadBackgroundShifters() {
+	p.BGShifterPatternLow = (p.BGShifterPatternLow & 0xFF00) | uint16(p.BGNextTileLSB)
+	p.BGShifterPatternHigh = (p.BGShifterPatternHigh & 0xFF00) | uint16(p.BGNextTileMSB)
+	if p.BGNextTileAttrib&0b01 != 0 {
+		p.BGShifterAttributeLow = (p.BGShifterAttributeLow & 0xFF00) | 0xFF
+	} else {
+		p.BGShifterAttributeLow = (p.BGShifterAttributeLow & 0xFF00) | 0x00
+	}
+
+	if p.BGNextTileAttrib&0b10 != 0 {
+		p.BGShifterAttributeHigh = (p.BGShifterAttributeHigh & 0xFF00) | 0xFF
+	} else {
+		p.BGShifterAttributeHigh = (p.BGShifterAttributeHigh & 0xFF00) | 0x00
+	}
+
+}
+
+func (p *PPU) UpdateShifters() {
+	if p.Mask.IsShowBG() {
+		p.BGShifterAttributeHigh <<= 1
+		p.BGShifterAttributeLow <<= 1
+		p.BGShifterPatternHigh <<= 1
+		p.BGShifterPatternLow <<= 1
+	}
+}
+
+func (p *PPU) Reset() {
+	p.FineX = 0x00
+	p.AddressLatch = false
+	p.InternalDataBuffer = 0x00
+	p.Scanline = 0
+	p.Cycles = 0
+	p.BGNextTileId = 0x00
+	p.BGNextTileAttrib = 0x00
+	p.BGNextTileMSB = 0x00
+	p.BGNextTileLSB = 0x00
+	p.BGShifterAttributeLow = 0x0000
+	p.BGShifterAttributeHigh = 0x0000
+	p.BGShifterPatternLow = 0x0000
+	p.BGShifterPatternHigh = 0x0000
+	p.Status.Status = 0x00
+	p.Mask.Status = 0x00
+	p.Controller.Status = 0x00
+	p.VRamAddr.Data = 0x0000
+	p.TRamAddr.Data = 0x0000
 }
